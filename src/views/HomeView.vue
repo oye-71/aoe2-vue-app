f6a0451e3953a59ef79bb70a5d392d5991d6c0d7
oye71 <etiennedumontet@hotmail.com>
2022-09-22 10:06:49 +0200

    Remove vue base components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
     <div>Fetch aoe returned :</div>
     <div v-if="leaderboard">
       <div v-for="leader of leaderboard.items">{{ leader.userName }} - {{ leader.elo }} elo</div>
